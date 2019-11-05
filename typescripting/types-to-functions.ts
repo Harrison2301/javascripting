3d18892d1da0d4196070996847cf9bcd3b695f91
Harrison2301 <harrisonwinters2301@gmail.com>
2019-11-05 05:30:00 +0000

    comments

--- typescripting/types-to-functions.ts
+++ typescripting/types-to-functions.ts
@@ -1,3 +1,5 @@
+
+// Typescript is essentially optional - just a super-set of JS. however should be used for cleaner and described code
 function add(num1: number, num2: number): string {
     return (num1 + num2).toString();
 }
--- typescripting/types-to-variables.ts
+++ typescripting/types-to-variables.ts
@@ -1,3 +1,4 @@
+// @ts-check can be used in JS files to check for errors
 const example: number[] = [1, 2, 3, 4, 5];
 
 let ex2 = example.map((element) => {
