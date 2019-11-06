dbbdce9dda13987eaa87c69dd94f07aabf55d1ea
Harrison2301 <harrisonwinters2301@gmail.com>
2019-11-06 08:10:00 +0000

    created a type numExample

--- typescripting/types-to-functions.ts
+++ typescripting/types-to-functions.ts
@@ -1,8 +1,10 @@
 
 // Typescript is essentially optional - just a super-set of JS. however should be used for cleaner and described code
 import { NumsEnum } from './example.enum';
-function add(num1: NumsEnum, num2: NumsEnum): string {
-    return (num1 + num2).toString();
+type numExample = { num1: number, num2: number}
+
+function add(numExample): number {
+    return nums.num1 + nums.num2;
 }
 
-add(NumsEnum.Num1, NumsEnum.Num2)
+add({num1: 1, num2: 2});
