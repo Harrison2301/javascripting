860b20612e49325aee5435aacf16504419bf5243
Harrison2301 <harrisonwinters2301@gmail.com>
2019-11-21 08:10:00 +0000

    solution 3 for rev string

--- javascript-cardio/reverseString.js
+++ javascript-cardio/reverseString.js
@@ -3,11 +3,17 @@ function reverseString(str) {
 // return str.split('').reverse().join('');
 
     // sol 2
+// let revString = '';
+// for(let i = str.length - 1; i >= 0; i--){
+//     revString = revString + str[i];
+// }
+// return revString;
+
+    // sol 3 
 let revString = '';
-for(let i = str.length - 1; i >= 0; i--){
-    revString = revString + str[i];
-}
-return revString;
+str.split('').forEach(char => revString = char +
+    revString);
+    return revString;
 }
 
 const output = reverseString('harrison');
