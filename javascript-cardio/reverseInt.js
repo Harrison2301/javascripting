3a10e66f15e6a66a39aa10d5cfe9a61d9e0f493e
Harrison2301 <harrisonwinters2301@gmail.com>
2019-11-25 08:10:00 +0000

    create function to reverse a set of intergers

---
+++ javascript-cardio/reverseInt.js
@@ -0,0 +1,8 @@
+function reverseInt(int){
+    const revString = int.toString().split('').reverse().join('');
+    return parseInt(revString) * Math.sign(int);
+}
+
+const output = reverseInt(-12345);
+
+console.log(output);
