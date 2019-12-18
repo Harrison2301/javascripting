66af4acb2648576a94df2b5ea933be2627dfca15
Harrison2301 <harrisonwinters2301@gmail.com>
2019-12-18 08:10:00 +0000

    fix display error, test number display

--- calculator/script.js
+++ calculator/script.js
@@ -1,25 +1,30 @@
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement){
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
     }
-    clear(){
-
+  
+    clear() {
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined
     }
     delete(){
 
     }
     appendNumber(number){
+        this.currentOperand = number
 
     }
-    choseOperation(operation){
+    chooseOperation(operation){
 
     }
-    computer(){
+    compute(){
 
     }
     updateDisplay(){
-        
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -29,3 +34,12 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
